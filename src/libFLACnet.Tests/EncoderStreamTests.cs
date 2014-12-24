17d8933c34028b53d8158108c0ad1e8812cf3f2f
Warren Kaufman <discomurray@gmail.com>
2014-12-24 07:51:56 -0700

    Add unit test project for libFLACnet

---
+++ src/libFLACnet.Tests/EncoderStreamTests.cs
@@ -0,0 +1,22 @@
+﻿namespace FLAC
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class EncoderStreamTests
+    {
+        [TestMethod]
+        public void Construction_IsValid()
+        {
+            EncoderStream stream = new EncoderStream();
+
+            Assert.IsTrue(stream.IsValid);
+        }
+    }
+}
