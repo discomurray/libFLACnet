062b5eb509a71a45ca8940d0f2378e48406e8500
Warren Kaufman <discomurray@gmail.com>
2014-12-24 08:59:19 -0700

    update formatting of property declarations.

--- src/libFLACnet/EncoderStream.h
+++ src/libFLACnet/EncoderStream.h
@@ -13,25 +13,64 @@ namespace FLAC
 
 		~EncoderStream();
 
-		property unsigned int BitsPerSample { unsigned int get(); void set(unsigned int value); }
+		property unsigned int BitsPerSample
+		{
+			unsigned int get();
+			void set(unsigned int value);
+		}
 
-		property unsigned int BlockSize { unsigned int get(); void set(unsigned int value); }
+		property unsigned int BlockSize
+		{
+			unsigned int get();
+			void set(unsigned int value);
+		}
 
-		property unsigned int Channels { unsigned int get(); void set(unsigned int value); }
+		property unsigned int Channels
+		{
+			unsigned int get();
+			void set(unsigned int value);
+		}
 
-		property bool EscapeCoding { bool get(); void set(bool value); }
+		property bool EscapeCoding
+		{
+			bool get();
+			void set(bool value);
+		}
 
-		property bool ExhaustiveModelSearch { bool get(); void set(bool value); }
+		property bool ExhaustiveModelSearch
+		{
+			bool get();
+			void set(bool value);
+		}
 
-		property bool IsValid { bool get(); }
+		property bool IsValid
+		{
+			bool get();
+		}
 
-		property unsigned int MaxLpcOrder { unsigned int get(); void set(unsigned int value); }
+		property unsigned int MaxLpcOrder
+		{
+			unsigned int get();
+			void set(unsigned int value);
+		}
 
-		property bool MidSideStereo { bool get(); void set(bool value); }
+		property bool MidSideStereo
+		{
+			bool get();
+			void set(bool value);
+		}
 
-		property unsigned int QlpCoeffPrecision { unsigned int get(); void set(unsigned int value); }
+		property unsigned int QlpCoeffPrecision
+		{
+			unsigned int get();
+			void set(unsigned int value);
+		}
 
-		property bool QlpCoeffPrecisionSearch { bool get(); void set(bool value); }
+		property bool QlpCoeffPrecisionSearch
+		{
+			bool get();
+			void set(bool value);
+		}
 
 		property unsigned int ResidualPartitionOrderMinimum
 		{
@@ -45,11 +84,23 @@ namespace FLAC
 			void set(unsigned int value);
 		}
 
-		property unsigned int SampleRate { unsigned int get(); void set(unsigned int); }
+		property unsigned int SampleRate
+		{
+			unsigned int get();
+			void set(unsigned int);
+		}
 
-		property bool StreamableSubset { bool get(); void set(bool value); }
+		property bool StreamableSubset
+		{
+			bool get();
+			void set(bool value);
+		}
 
-		property bool Verify { bool get(); void set(bool value); }
+		property bool Verify
+		{
+			bool get();
+			void set(bool value);
+		}
 
 		void SetApodization(System::String^ specification);
 
